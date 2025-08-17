25df3d3ab6ec4372b8ce53600661d621884fc131
albina_kazartseva <albinak74@gmail.com>
2025-08-17 12:13:31 +0500

    nginx

--- config/settings.py
+++ config/settings.py
@@ -127,14 +127,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# STATICFILES_DIRS = [
-#     # Здесь вы можете добавить пути к вашим статическим файлам,
-#     # если они располагаются вне приложений
-#     BASE_DIR
-#     / "static",
-# ]
+STATICFILES_DIRS = [
+    # Здесь вы можете добавить пути к вашим статическим файлам,
+    # если они располагаются вне приложений
+    BASE_DIR
+    / "static",
+]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
